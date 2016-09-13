2138b3ac43745880665562091b9eec5b764fdb38
Josh Borrow <josh.borrow@gmail.com>
2016-09-13 09:00:46 +0100

    added module documentation, init imports.

---
+++ ltmd/__init__.py
@@ -0,0 +1,15 @@
+"""
+This is a LaTeX -> Markdown preprocessor. It uses regex to extract certain
+parts of the LaTeX that are not handled 'correctly' by John McFarlane's 
+Pandoc, replaces them with a unique string and prcoesses them separately,
+whilst the main body of text is converted with Pandoc.
+
+For more information, view the README or check out the github page:
+
+    https://www.github.com/jborrow/latex-pandoc-preprocessor.
+
+Author: Josh Borrow.
+"""
+
+from parser import *
+from inputoutput import *
