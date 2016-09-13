6af7e70d483a95ef6a9fc9a2312cfcd0d4174009
Josh Borrow <josh.borrow@gmail.com>
2016-09-13 09:39:16 +0100

    moved to accepting command-line arguments

---
+++ preprocess.py
@@ -0,0 +1,38 @@
+"""
+Comamnd-line access for ltmd.
+
+Use:
+
+    python3 preprocess.py <INPUT> <OUTPUT>
+
+Takes an input of a LaTeX file, outputs a Pandoc Markdown file.
+"""
+
+import argparse
+import ltmd
+import os
+
+
+# deal with command line options
+
+Parser = argparse.ArgumentParser()
+
+Parser.add_argument("input", type=str, help='Filename of the input .tex file.')
+Parser.add_argument('output', type=str, help='Output markdown filename.')
+
+Args = Parser.parse_args()
+
+
+InputFilename = Args.input
+OutputFilename = Args.output
+
+
+# now need to run ltmd options
+
+InputText = ltmd.GetTex(InputFilename)
+
+PreProcessed = ltmd.PreProcess(InputText)
+Pandocced = ltmd.RunPandoc(PreProcessed.ParsedText)
+PostProcessed = ltmd.PostProcess(Pandocced, PreProcessed.ParsedData)
+
+Output = ltmd.OutputMD(OutputFilename, PostProcessed.ParsedText)
--- test.py
+++
@@ -1,9 +0,0 @@
-import ltmd
-
-input = ltmd.GetTex('Nuclear.tex')
-
-PreProcessed = ltmd.PreProcess(input)
-pandocced = ltmd.RunPandoc(PreProcessed.ParsedText)
-PostProcessed = ltmd.PostProcess(pandocced, PreProcessed.ParsedData)
-
-output = ltmd.OutputMD('test.md', PostProcessed.ParsedText) 
