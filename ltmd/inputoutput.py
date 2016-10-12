aab20eb22ab018b3525bd4d943cd14605056d054
Josh Borrow <josh.borrow@gmail.com>
2016-10-12 14:46:27 +0100

    removed the need for temporary files using pypandoc

--- ltmd/inputoutput.py
+++ ltmd/inputoutput.py
@@ -8,6 +8,7 @@ Date Created: 2016-09-09
 import os
 import tempfile
 import subprocess
+import pypandoc
 
 
 def GetTex(filename):
@@ -29,23 +30,8 @@ def OutputMD(filename, content):
 def RunPandoc(content, extra=[]):
     """ Creates a temporary file, runs pandoc TeX->MD on it (with content)
     and then reopens and returns the string. """
-
-    TempFileTeX = tempfile.NamedTemporaryFile(mode='w+t', encoding='utf-8')
-    TempFileMD = tempfile.NamedTemporaryFile(mode='w+t', encoding='utf-8')
-
-    print("Writing temporary LaTeX file")
-    TempFileTeX.write(content)
+    print("Running Pandoc (LT -> MD)")
+    OutputData = pypandoc.convert_text(content, "md", format="latex", extra_args=extra)
     
-    TeXContents = TempFileTeX.read()
-
-    print("Running Pandoc")
-    subprocess.call(['pandoc', '-f', 'latex', '-t', 'markdown', TempFileTeX.name, '-o', TempFileMD.name] + extra)
-
-    print("Reading temporary output file")
-    OutputData = TempFileMD.read()
-
-    TempFileTeX.close()
-    TempFileMD.close()
-
     return OutputData
 
