f7bef37a1d2f317839daef6590143614d265d125
Josh Borrow <joshua.borrow@durham.ac.uk>
2017-06-12 09:24:47 +0100

    Formatting issues resolved

--- ltmd/inputoutput.py
+++ ltmd/inputoutput.py
@@ -10,10 +10,10 @@ import pypandoc
 
 def get_tex(filename):
     """ Get the content of the tex file """
-    
+
     print("Reading {}...".format(filename))
     with open(filename, 'r') as file:
-       return file.read()
+        return file.read()
 
 
 def output_md(filename, content):
@@ -28,7 +28,7 @@ def run_pandoc(content, extra=[]):
     """ Creates a temporary file, runs pandoc TeX->MD on it (with content)
     and then reopens and returns the string. """
     print("Running Pandoc (LT -> MD)")
-    OutputData = pypandoc.convert_text(content, "md", format="latex", extra_args=extra)
-    
-    return OutputData
+    output = pypandoc.convert_text(content, "md", format="latex", extra_args=extra)
+
+    return output
 
